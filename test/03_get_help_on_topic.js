4dc8bf46fac2f7361c3275d93c30796bac453018
Olivier ORABONA <oorabona@users.noreply.github.com>
2021-06-05 09:04:35 +0200

    use api `setup` to reset params instead of creating new objects

--- test/03_get_help_on_topic.js
+++ test/03_get_help_on_topic.js
@@ -5,7 +5,7 @@ const example = {
   help: {
     help: 'this help or additional help on a given topic',
     short: '((?<topic>[\\w|\\/]+) )?-h',
-    long: 'help( (?<topic>[\\w|\\/]+))?',
+    long: 'help( (?<topic>[\\w|\\/]+))?'
   }
 }
 
@@ -13,7 +13,7 @@ const exampleWithOverride = {
   help: {
     help: 'this help or additional help on a given topic',
     short: '((?<help>[\\w|\\/]+) )?-h',
-    long: 'help( (?<help>[\\w|\\/]+))?',
+    long: 'help( (?<help>[\\w|\\/]+))?'
   }
 }
 
@@ -29,8 +29,8 @@ const exampleWithStopParse = {
 const exampleWithDefaultValue = {
   help: {
     help: 'this help or additional help on a given topic',
-    short: /((?<topic>[\w|\/]+) )?-h/,
-    long: /help( (?<topic>[\w|\/]+))?/,
+    short: /((?<topic>[\w|/]+) )?-h/,
+    long: /help( (?<topic>[\w|/]+))?/,
     values: {
       topic: 'general'
     }
@@ -43,21 +43,6 @@ const customDebugFn = (...args) => {
 }
 
 const myArgs = new Reargs(example, {
-  longShortDelimiter: ', ',
-  paramDescriptionSpacer: ' '
-}, customDebugFn)
-
-const myArgsWithOverride = new Reargs(exampleWithOverride, {
-  longShortDelimiter: ', ',
-  paramDescriptionSpacer: ' '
-}, customDebugFn)
-
-const myArgsWithStopParse = new Reargs(exampleWithStopParse, {
-  longShortDelimiter: ', ',
-  paramDescriptionSpacer: ' '
-}, customDebugFn)
-
-const myArgsWithDefaultValue = new Reargs(exampleWithDefaultValue, {
   longShortDelimiter: ', ',
   paramDescriptionSpacer: ' '
 }, customDebugFn)
@@ -130,69 +115,70 @@ describe('Testing advanced use of capture groups (without stopParse)', () => {
   })
 })
 
-describe('Testing advanced use of capture groups (captue group have the same name as parameter)', () => {
+describe('Testing advanced use of capture groups (capture group have the same name as parameter)', () => {
   it('should be able to process only "help"', () => {
-    const unparsable = myArgsWithOverride.parse(['help'])
-    const valueOfParam = myArgsWithOverride.getValue('help', 'help')
+    assert.strictEqual(myArgs.setup(exampleWithOverride), true)
+    const unparsable = myArgs.parse(['help'])
+    const valueOfParam = myArgs.getValue('help', 'help')
 
     assert.strictEqual(unparsable, '')
     assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, undefined)
-    assert.strictEqual(myArgsWithOverride.getValue(), null)
-    assert.deepEqual(myArgsWithOverride.getValue('help'), {
+    assert.strictEqual(myArgs.getValue(), null)
+    assert.deepEqual(myArgs.getValue('help'), {
       help: undefined
     })
-    assert.deepEqual(myArgsWithOverride.getGroupValues(), {})
-    assert.deepEqual(myArgsWithOverride.getGroupValues('_'), {
+    assert.deepEqual(myArgs.getGroupValues(), {})
+    assert.deepEqual(myArgs.getGroupValues('_'), {
       help: undefined
     })
-    assert.deepEqual(myArgsWithOverride.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       help: undefined
     })
   })
 
   it('should be able to process "help foo"', () => {
-    const unparsable = myArgsWithOverride.parse(['help', 'foo'])
-    const valueOfParam = myArgsWithOverride.getValue('help', 'help')
+    const unparsable = myArgs.parse(['help', 'foo'])
+    const valueOfParam = myArgs.getValue('help', 'help')
 
     // assert.strictEqual(message, '')
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithOverride.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, 'foo')
-    assert.strictEqual(myArgsWithOverride.getValue(), null)
-    assert.deepEqual(myArgsWithOverride.getValue('help'), {
+    assert.strictEqual(myArgs.getValue(), null)
+    assert.deepEqual(myArgs.getValue('help'), {
       help: 'foo'
     })
-    assert.deepEqual(myArgsWithOverride.getGroupValues(), {})
-    assert.deepEqual(myArgsWithOverride.getGroupValues('_'), {
+    assert.deepEqual(myArgs.getGroupValues(), {})
+    assert.deepEqual(myArgs.getGroupValues('_'), {
       help: 'foo'
     })
-    assert.deepEqual(myArgsWithOverride.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       help: 'foo'
     })
   })
 
   it('should be able to process only "-h"', () => {
-    const unparsable = myArgsWithOverride.parse(['-h'])
-    const valueOfParam = myArgsWithOverride.getValue('help', 'help')
+    const unparsable = myArgs.parse(['-h'])
+    const valueOfParam = myArgs.getValue('help', 'help')
 
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithOverride.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, undefined)
-    assert.deepEqual(myArgsWithOverride.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       help: undefined
     })
   })
 
   it('should be able to process "foo -h"', () => {
-    const unparsable = myArgsWithOverride.parse(['foo', '-h'])
-    const valueOfParam = myArgsWithOverride.getValue('help', 'help')
+    const unparsable = myArgs.parse(['foo', '-h'])
+    const valueOfParam = myArgs.getValue('help', 'help')
 
     // assert.strictEqual(message, '')
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithOverride.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, 'foo')
-    assert.deepEqual(myArgsWithOverride.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       help: 'foo'
     })
   })
@@ -201,51 +187,52 @@ describe('Testing advanced use of capture groups (captue group have the same nam
 describe('Testing advanced use of capture groups (with stopParse)', () => {
   it('should be able to process only "help"', () => {
     message = ''
-    const unparsable = myArgsWithStopParse.parse(['help'])
-    const valueOfParam = myArgsWithStopParse.getValue('help', 'topic')
+    assert.strictEqual(myArgs.setup(exampleWithStopParse), true)
+    const unparsable = myArgs.parse(['help'])
+    const valueOfParam = myArgs.getValue('help', 'topic')
 
     // assert.strictEqual(message, '')
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithStopParse.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, undefined)
-    assert.deepEqual(myArgsWithStopParse.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       topic: undefined
     })
   })
 
   it('should be able to process "help foo"', () => {
-    const unparsable = myArgsWithStopParse.parse(['help', 'foo'])
-    const valueOfParam = myArgsWithStopParse.getValue('help', 'topic')
+    const unparsable = myArgs.parse(['help', 'foo'])
+    const valueOfParam = myArgs.getValue('help', 'topic')
 
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithStopParse.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, 'foo')
-    assert.deepEqual(myArgsWithStopParse.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       topic: 'foo'
     })
   })
 
   it('should be able to process only "-h"', () => {
-    const unparsable = myArgsWithStopParse.parse(['-h'])
-    const valueOfParam = myArgsWithStopParse.getValue('help', 'topic')
+    const unparsable = myArgs.parse(['-h'])
+    const valueOfParam = myArgs.getValue('help', 'topic')
 
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithStopParse.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, undefined)
-    assert.deepEqual(myArgsWithStopParse.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       topic: undefined
     })
   })
 
   it('should be able to process "foo -h"', () => {
-    const unparsable = myArgsWithStopParse.parse(['foo', '-h'])
-    const valueOfParam = myArgsWithStopParse.getValue('help', 'topic')
+    const unparsable = myArgs.parse(['foo', '-h'])
+    const valueOfParam = myArgs.getValue('help', 'topic')
 
     // assert.strictEqual(message, '')
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithStopParse.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, 'foo')
-    assert.deepEqual(myArgsWithStopParse.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       topic: 'foo'
     })
   })
@@ -254,51 +241,52 @@ describe('Testing advanced use of capture groups (with stopParse)', () => {
 describe('Testing advanced use of capture groups (with default value)', () => {
   it('should be able to process only "help"', () => {
     message = ''
-    const unparsable = myArgsWithDefaultValue.parse(['help'])
-    const valueOfParam = myArgsWithDefaultValue.getValue('help', 'topic')
+    assert.strictEqual(myArgs.setup(exampleWithDefaultValue), true)
+    const unparsable = myArgs.parse(['help'])
+    const valueOfParam = myArgs.getValue('help', 'topic')
 
     // assert.strictEqual(message, '')
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithDefaultValue.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, 'general')
-    assert.deepEqual(myArgsWithDefaultValue.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       topic: 'general'
     })
   })
 
   it('should be able to process "help foo"', () => {
-    const unparsable = myArgsWithDefaultValue.parse(['help', 'foo'])
-    const valueOfParam = myArgsWithDefaultValue.getValue('help', 'topic')
+    const unparsable = myArgs.parse(['help', 'foo'])
+    const valueOfParam = myArgs.getValue('help', 'topic')
 
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithDefaultValue.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, 'foo')
-    assert.deepEqual(myArgsWithDefaultValue.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       topic: 'foo'
     })
   })
 
   it('should be able to process only "-h"', () => {
-    const unparsable = myArgsWithDefaultValue.parse(['-h'])
-    const valueOfParam = myArgsWithDefaultValue.getValue('help', 'topic')
+    const unparsable = myArgs.parse(['-h'])
+    const valueOfParam = myArgs.getValue('help', 'topic')
 
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithDefaultValue.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, 'general')
-    assert.deepEqual(myArgsWithDefaultValue.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       topic: 'general'
     })
   })
 
   it('should be able to process "foo -h"', () => {
-    const unparsable = myArgsWithDefaultValue.parse(['foo', '-h'])
-    const valueOfParam = myArgsWithDefaultValue.getValue('help', 'topic')
+    const unparsable = myArgs.parse(['foo', '-h'])
+    const valueOfParam = myArgs.getValue('help', 'topic')
 
     // assert.strictEqual(message, '')
     assert.strictEqual(unparsable, '')
-    assert.strictEqual(myArgsWithDefaultValue.remain, '')
+    assert.strictEqual(myArgs.remain, '')
     assert.strictEqual(valueOfParam, 'foo')
-    assert.deepEqual(myArgsWithDefaultValue.getAllValues(), {
+    assert.deepEqual(myArgs.getAllValues(), {
       topic: 'foo'
     })
   })
